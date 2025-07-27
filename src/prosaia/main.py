da8711b6154540c8de247a17db5f07b0a3a2b798
Gabriel <carloscgmorais@gmail.com>
2025-07-27 16:17:08 -0300

    configuração da api de modelo generativo

--- src/prosaia/main.py
+++ src/prosaia/main.py
@@ -1,23 +1,32 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
 from fastapi import FastAPI, WebSocket, Request
 from fastapi.templating import Jinja2Templates
 
+load_dotenv()
+
 app = FastAPI()
 
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates('templates')
 
+API_KEY = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=API_KEY) #type: ignore
+
+model = genai.GenerativeModel('models/gemini-2.5-pro') #type: ignore
 
 @app.get('/')
-async def get(request: Request):
-    return templates.TemplateResponse('index.html',{'request':request})
+async def get(request : Request):
+    return templates.TemplateResponse('index.html',{'request': request})
 
 
 @app.websocket('/ws')
-async def websocket_endpoint(websocket : WebSocket):
+async def endpoint_websocket(websocket: WebSocket):
     await websocket.accept()
-
-    while True :
-       data = await websocket.receive_text()
-
-       await websocket.send_text(f'Mensagem recebida: {data}')
-
-
+    try :
+        while True:
+            data = await websocket.receive_text()
+            response = model.generate_content(data)
+            await websocket.send_text(response.text)
+    except Exception as erro:
+        print(f"Deu erro no websocket: {erro}")
