6881e0e17ab74d96e6885d41dfe566f697b87028
Gabriel <carloscgmorais@gmail.com>
2025-08-09 20:43:00 -0300

    refactor main.py and index.html for improved structure and styling

--- src/prosaia/main.py
+++ src/prosaia/main.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
-templates = Jinja2Templates('templates')
-
 load_dotenv()
+app = FastAPI()
+
+templates = Jinja2Templates('templates')
 API_KEY = os.getenv('GOOGLE_API_KEY')
+
 genai.configure(api_key=API_KEY)  # type: ignore
 model = genai.GenerativeModel('models/gemini-2.5-pro')  # type: ignore
 
@@ -20,14 +21,20 @@ async def get(request: Request):
 
 
 @app.websocket('/ws')
-async def endpoint_websocket(websocket: WebSocket):
+async def end_point_websocket(websocket: WebSocket):
     await websocket.accept()
+    conversation_history = []
     try:
         while True:
-            data = await websocket.receive_text()
-            response_stream = model.generate_content(data, stream=True)
+            content = await websocket.receive_text()
+            conversation_history.append(f'usuario: {content}')
+            context  = '\n'.join(conversation_history)+'\nAssistente:'
+            response_stream = model.generate_content(context, stream=True)
+            response_model = ''
             for chunk in response_stream:
                 if chunk.text:
                     await websocket.send_text(chunk.text)
+                    response_model += chunk.text
+            conversation_history.append(f'Assistente:{response_model}')
     except Exception as erro:
-        print(f'Erro no websocket: {erro}')
+        print(f'erro no websocket: {erro}')
